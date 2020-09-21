816396658d679394d44683f5facc2e8129953f2c
VitorNevess <j.victtor.neves@hotmail.com>
2020-09-20 22:35:53 -0300

    remocao do comentario

--- Red-A-Arara/Assets/Scripts/EspinhoMorte.cs
+++ Red-A-Arara/Assets/Scripts/EspinhoMorte.cs
@@ -7,8 +7,7 @@ public class EspinhoMorte : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //red a arara
-        //sei la
+        
     }
 
     // Update is called once per frame
