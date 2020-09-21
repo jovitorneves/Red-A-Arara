a5d4f7f4da8431f6dbd7608130ba1ded7e09c0d4
NeoFullBurst <fullburstkaio@gmail.com>
2020-09-20 21:11:07 -0300

    comentario teste

--- Red-A-Arara/Assets/Scripts/EspinhoMorte.cs
+++ Red-A-Arara/Assets/Scripts/EspinhoMorte.cs
@@ -7,7 +7,7 @@ public class EspinhoMorte : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //red a arara
     }
 
     // Update is called once per frame
