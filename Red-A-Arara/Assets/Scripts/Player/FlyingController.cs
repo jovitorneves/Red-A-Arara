a263d615ee5a0581798a8e607d323b62b3995a7b
VitorNevess <j.victtor.neves@hotmail.com>
2021-01-25 13:07:29 -0300

    troca de classes

---
+++ Red-A-Arara/Assets/Scripts/Player/FlyingController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingController : MonoBehaviour
+{
+
+    public float horizontalSpeed = 0.08f;
+    public float verticalSpeed = 1.0f;
+    public float height = 1.0f;
+
+    public Vector3 tempPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tempPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        return;
+        tempPosition.x += horizontalSpeed;
+        tempPosition.y += Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * height;
+        transform.position = tempPosition;
+    }
+}
