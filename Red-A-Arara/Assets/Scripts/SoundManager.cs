82841de7c0f09331807b9f470f334789710f9490
VitorNevess <j.victtor.neves@hotmail.com>
2021-01-30 18:06:21 -0300

    Musica esta muito alta #23

--- Red-A-Arara/Assets/Scripts/SoundManager.cs
+++ Red-A-Arara/Assets/Scripts/SoundManager.cs
@@ -5,7 +5,6 @@ public class SoundManager : GenericSingletonClass<SoundManager>
 
     public AudioSource fxPlayer;
     public AudioSource fxBuritiCollector;
-
     public AudioSource fxCobraAttack;
     public AudioSource fxCobraDie;
     public AudioSource fxCobraChefeDamageTaken;
@@ -14,29 +13,34 @@ public class SoundManager : GenericSingletonClass<SoundManager>
     {
         fxPlayer.clip = clip;
         fxPlayer.Play();
+        fxPlayer.volume = 0.055f;
     }
 
     public void PlayFxBuritiCollector (AudioClip clip)
     {
         fxBuritiCollector.clip = clip;
         fxBuritiCollector.Play();
+        fxBuritiCollector.volume = 0.051f;
     }
 
     public void PlayFxCobraAttack (AudioClip clip)
     {
         fxCobraAttack.clip = clip;
         fxCobraAttack.Play();
+        fxCobraAttack.volume = 0.555f;
     }
 
     public void PlayFxCobraDie (AudioClip clip)
     {
         fxCobraDie.clip = clip;
         fxCobraAttack.Play();
+        fxCobraAttack.volume = 0.348f;
     }
     
     public void PlayFxCobraChefeDamageTaken (AudioClip clip)
     {
         fxCobraChefeDamageTaken.clip = clip;
         fxCobraChefeDamageTaken.Play();
+        fxCobraChefeDamageTaken.volume = 0.318f;
     }
 }
