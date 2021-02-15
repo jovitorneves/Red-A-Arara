0331b64fe23f92c2fdc6934e47a9fb6dfa4f6d77
VitorNevess <j.victtor.neves@hotmail.com>
2021-02-15 17:48:52 -0300

    Implementation - Criação das animações #20 Humano andando Humano capturando Porta abrindo e fechando Tucano andando Tucano voando

--- Red-A-Arara/Assets/Scripts/Common/Tags/AnimationTagsConstants.cs
+++ Red-A-Arara/Assets/Scripts/Common/Tags/AnimationTagsConstants.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public static class AnimationTagsConstants
 {
+    //RED
     public const string Voando = "FLYING";
     public const string MorteRed = "Morta";
     public const string ParadaRed = "RedParada";
     public const string AndandoRed = "RedAndando";
     public const string PulandoRed = "RedPulando";
     public const string WinRed = "RedWin";
+
+    //HUMANO
+    public const string IdleHumano = "IdleHumano";
+    public const string CapturaHumano = "CapturaHumano";
+
+    //TUCANO
+    public const string IdleTucano = "IdleTucano";
+    public const string VoandoTucano = "VoandoTucano";
+
+    //Porta
+    public const string OpenClosePorta = "AbrindoFechandoPorta";
+
+    //GERAL
     public const string Death = "DEATH";
     public const string Walk = "WALK";//WALK //Walk
     public const string Idle = "IDLE";//IDLE //Idle
     public const string Jump = "JUMP";//JUMP //Jump
+
+    //COBRA
     public const string LevandoDano = "TAKENDAMAGE";//TAKENDAMAGE //TakenDamage
 }
