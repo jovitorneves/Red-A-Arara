b2c5a031e8407a91355dff032ac735c3e459441b
VitorNevess <j.victtor.neves@hotmail.com>
2021-02-16 09:41:33 -0300

    Mecanica da porta feita

--- Red-A-Arara/Assets/Scripts/Itens/BotaoController.cs
+++ Red-A-Arara/Assets/Scripts/Itens/BotaoController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class BotaoController : MonoBehaviour
 {
+    [SerializeField]
+    private Animator portaAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        portaAnimator = portaAnimator.gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (UtilController.Instance.ReturnDirection(other.contacts) != HitDirection.Top)
+            return;
+
+        portaAnimator.enabled = true;
+        portaAnimator.Play(AnimationTagsConstants.OpenClosePorta);
     }
 }
