b0b8bfc619ce21da52d9b5ae958ceec2cefcaa83
VitorNevess <j.victtor.neves@hotmail.com>
2021-02-21 19:24:10 -0300

    Humano refatorado

--- Red-A-Arara/Assets/Scripts/Humano/HumanoController.cs
+++ Red-A-Arara/Assets/Scripts/Humano/HumanoController.cs
@@ -2,68 +2,72 @@
 
 public class HumanoController : BaseEnemyController
 {
+    //Player
     [SerializeField]
     private GameObject player;
-
-    [SerializeField]
-    private Transform posicaoA, posicaoB;
-
-    private Animator animator;
-
     private Player playerScript;
 
+    //Humano
     [SerializeField]
-    private bool isLookLeft = false;
+    private float moveSpeed = 1f;
+    [SerializeField]
+    private GameObject pointA;
+    [SerializeField]
+    private GameObject pointB;
+    [SerializeField]
+    private bool reverseMove = false;
+    [SerializeField]
+    private Transform objectToUse;
+    [SerializeField]
+    private bool moveThisObject = false;
 
     private readonly float distanciaHumanoAndPointMax = 0.1f;
+    private Animator animator;
+    private float startTime;
+    private float journeyLength;
+    private float distCovered;
+    private float fracJourney;
 
-    private readonly float speed = 1f;
-
-    private Vector3 nextPos;
-
-    // Start is called before the first frame update
     void Start()
     {
         playerScript = player.GetComponent<Player>();
         animator = GetComponent<Animator>();
 
-        if (posicaoA.Equals(null) || posicaoB.Equals(null))
-            return;
+        startTime = Time.time;
 
-        posicaoA.position = new Vector3(posicaoA.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
-        posicaoB.position = new Vector3(posicaoB.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+        if (moveThisObject)
+            objectToUse = transform;
+
+        journeyLength = Vector3.Distance(pointA.transform.position, pointB.transform.position);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (posicaoA.Equals(null) || posicaoB.Equals(null))
+        distCovered = (Time.time - startTime) * moveSpeed;
+        fracJourney = distCovered / journeyLength;
+
+        if (pointA.Equals(null) || pointB.Equals(null))
             return;
 
-        float disA = gameObject.transform.position.x - posicaoA.position.x;
-        float disB = gameObject.transform.position.x - posicaoB.position.x;
+        if (!playerScript.isAlive) return;
+
+        float disA = gameObject.transform.position.x - pointA.transform.position.x;
+        float disB = gameObject.transform.position.x - pointB.transform.position.x;
         disA = disA > 0 ? (disA * 1) : disA * -1;
         disB = disB > 0 ? (disB * 1) : disB * -1;
 
-        if (playerScript.isAlive)
-        {
-            if (player.gameObject.transform.position.x >= posicaoA.position.x &&
-                player.gameObject.transform.position.x <= posicaoB.position.x)
-            {
-                if (disA >= distanciaHumanoAndPointMax &&
-                    disB >= distanciaHumanoAndPointMax)
-                    CapturaRule();
-                else
-                    MoveHumano();
-            }
-            else
-                MoveHumano();
-        }
+        if (player.gameObject.transform.position.x >= pointA.transform.position.x &&
+            player.gameObject.transform.position.x <= pointB.transform.position.x &&
+            disA >= distanciaHumanoAndPointMax && disB >= distanciaHumanoAndPointMax)
+            MoveHumano(isCaptura: true);
+        else
+            MoveHumano(isCaptura: false);
+
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawLine(posicaoA.position, posicaoB.position);
+        Gizmos.DrawLine(pointA.transform.position, pointB.transform.position);
     }
 
     private void OnCollisionEnter2D(Collision2D collision2D)
@@ -72,48 +76,29 @@ public class HumanoController : BaseEnemyController
         //{
         //    
         //}
-
     }
 
-    private void MoveHumano()
+    private void MoveHumano(bool isCaptura)
     {
-        if (transform.position == posicaoA.position)
+        if (reverseMove)
+            objectToUse.position = Vector3.Lerp(pointB.transform.position, pointA.transform.position, fracJourney);
+        else
+            objectToUse.position = Vector3.Lerp(pointA.transform.position, pointB.transform.position, fracJourney);
+
+        if (Vector3.Distance(objectToUse.position, pointB.transform.position) == 0.0f ||
+            Vector3.Distance(objectToUse.position, pointA.transform.position) == 0.0f) //Checks if the object has travelled to one of the points
         {
-            nextPos = posicaoB.position;
-            Flip();
-        }
-        if (transform.position == posicaoB.position)
-        {
-            nextPos = posicaoA.position;
-            Flip();
-        }
-
-        transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
-        animator.Play(AnimationTagsConstants.IdleHumano);
-    }
-
-    private void CapturaRule()
-    {
-        if (!playerScript.isAlive) return;
-
-        if (player.gameObject.transform.position.x < posicaoA.position.x ||
-            player.gameObject.transform.position.x > posicaoB.position.x)
-            return;
-
-        animator.Play(AnimationTagsConstants.CapturaHumano);
-
-        if (transform.position == posicaoA.position)
-        {
-            nextPos = posicaoB.position;
-            Flip();
-        }
-        if (transform.position == posicaoB.position)
-        {
-            nextPos = posicaoA.position;
-            Flip();
+            if (reverseMove)
+                Flip();
+            else
+                Flip();
+            startTime = Time.time;
         }
 
-        transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+        if (isCaptura)
+            animator.Play(AnimationTagsConstants.CapturaHumano);
+        else
+            animator.Play(AnimationTagsConstants.IdleHumano);
     }
 
     private void Flip()
@@ -121,6 +106,6 @@ public class HumanoController : BaseEnemyController
         Vector3 newScale = gameObject.transform.localScale;
         newScale.x *= -1;
         gameObject.transform.localScale = newScale;
-        isLookLeft = !isLookLeft;
+        reverseMove = !reverseMove;
     }
 }
