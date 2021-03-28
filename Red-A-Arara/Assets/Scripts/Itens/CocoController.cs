2fcd682904797d362cd24d230038500f5d77e771
VitorNevess <j.victtor.neves@hotmail.com>
2021-03-28 16:06:31 -0300

    coco seguindo a plataforma e com vida

--- Red-A-Arara/Assets/Scripts/Itens/CocoController.cs
+++ Red-A-Arara/Assets/Scripts/Itens/CocoController.cs
@@ -4,10 +4,17 @@ public class CocoController : MonoBehaviour
 {
     private int lifeCount = 0;
 
+    private Rigidbody2D cocoRB;
+    private BoxCollider2D cocoBoxCollider;
+
     // Start is called before the first frame update
     void Start()
     {
+        cocoRB = GetComponent<Rigidbody2D>();
+        cocoBoxCollider = GetComponent<BoxCollider2D>();
 
+        cocoBoxCollider.sharedMaterial.friction = 0f;
+        cocoBoxCollider.sharedMaterial.bounciness = 0.4f;
     }
 
     // Update is called once per frame
@@ -27,6 +34,13 @@ public class CocoController : MonoBehaviour
             other.gameObject.CompareTag(TagsConstants.Espinhos) ||
             other.gameObject.CompareTag(TagsConstants.Player))
             Destroy(gameObject);
+
+        if (other.gameObject.CompareTag(TagsConstants.Botao))
+        {
+            cocoBoxCollider.sharedMaterial.friction = 0f;
+            cocoBoxCollider.sharedMaterial.bounciness = 0f;
+            cocoRB.velocity = new Vector2();
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -35,9 +49,29 @@ public class CocoController : MonoBehaviour
             Destroy(gameObject);
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(TagsConstants.Botao))
+        {
+            cocoBoxCollider.sharedMaterial.friction = 0f;
+            cocoBoxCollider.sharedMaterial.bounciness = 0f;
+            cocoRB.mass = 5f;
+        }
+    }
+
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag(TagsConstants.Rio))
+        if (collision.gameObject.CompareTag(TagsConstants.Botao))
+        {
+            cocoBoxCollider.sharedMaterial.friction = 0f;
+            cocoBoxCollider.sharedMaterial.bounciness = 0f;
+            cocoRB.mass = 5f;
+        }
+
+        if (collision.gameObject.CompareTag(TagsConstants.Rio) ||
+            collision.gameObject.CompareTag(TagsConstants.Espinhos) ||
+            collision.gameObject.CompareTag(TagsConstants.Player))
             Destroy(gameObject);
+
     }
 }
