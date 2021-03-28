744d15252496b199589c33df676e139778b11371
VitorNevess <j.victtor.neves@hotmail.com>
2021-03-28 11:49:01 -0300

    coco com vida

--- Red-A-Arara/Assets/Scripts/Itens/CocoController.cs
+++ Red-A-Arara/Assets/Scripts/Itens/CocoController.cs
@@ -2,6 +2,8 @@
 
 public class CocoController : MonoBehaviour
 {
+    private int lifeCount = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,14 +13,31 @@ public class CocoController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (lifeCount >= 2)
+            Destroy(gameObject);
     }
 
     void OnCollisionStay2D(Collision2D other)
     {
-        if (other.gameObject.CompareTag(TagsConstants.Player) ||
-            other.gameObject.CompareTag(TagsConstants.Enemy) ||
+        if (other.gameObject.CompareTag(TagsConstants.Enemy) ||
             other.gameObject.CompareTag(TagsConstants.Humano))
+            lifeCount++;
+
+        if (other.gameObject.CompareTag(TagsConstants.Rio) ||
+            other.gameObject.CompareTag(TagsConstants.Espinhos) ||
+            other.gameObject.CompareTag(TagsConstants.Player))
+            Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(TagsConstants.Rio))
+            Destroy(gameObject);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(TagsConstants.Rio))
             Destroy(gameObject);
     }
 }
