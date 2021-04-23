3ecb78410e9a3c99cd233c2190f027cd7f1a2aa0
VitorNevess <j.victtor.neves@hotmail.com>
2021-04-23 06:33:29 -0300

    parede da rocha criado

---
+++ Red-A-Arara/Assets/Scripts/Itens/Rocha/RochaController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RochaController : MonoBehaviour
+{
+
+    private Rigidbody2D rb2D;
+    [SerializeField]
+    private float speed = 3.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb2D.velocity = new Vector2(1 * speed, rb2D.velocity.y);
+    }
+}
--- Red-A-Arara/Assets/Scripts/Itens/Rocha/RochaParedeController.cs
+++ Red-A-Arara/Assets/Scripts/Itens/Rocha/RochaParedeController.cs
@@ -2,21 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RochaController : MonoBehaviour
+public class RochaParedeController : MonoBehaviour
 {
-
-    private Rigidbody2D rb2D;
-    private float speed = 2.5f;
-
     // Start is called before the first frame update
     void Start()
     {
-        rb2D = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb2D.velocity = new Vector2(1 * speed, rb2D.velocity.y);
+        
     }
 }
